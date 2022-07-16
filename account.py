0356b3fe2baf84a41c9acdb118a349215a317a48
brednan <brendanshauncaldwell@gmail.com>
2022-07-15 21:30:43 -0700

    Made the mechanisms to login to the account

---
+++ account.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+from requests import Session
+import requests
+
+
+class Account(Session):
+    def __init__(self, username, password):
+        self.FAILED_LOGIN = 0
+        self.NO_CONNECTION = 1
+        self.SUCCESSFUL_LOGIN = 2
+
+        Session.__init__(self)
+
+        self.headers_ = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'
+        }
+
+        raw_html_content = self.get('https://www.reddit.com/login', headers=self.headers_).text
+
+        soup = BeautifulSoup(raw_html_content, 'html.parser')
+
+        self.csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
+
+        self.login_payload = {
+            'csrf_token': self.csrf_token,
+            'otp': '',
+            'password': f'{password}',
+            'username': f'{username}',
+            'dest': 'https://www.reddit.com'
+        }
+
+    def login_request(self, timeout):
+        proxy_scheme = {
+            'http': 'http://geo.iproyal.com:12323',
+            'https': 'http://geo.iproyal.com:12323'
+        }
+
+        attempt = 0
+
+        while True:
+            try:
+                login_req = self.post(url='https://www.reddit.com/login', data=self.login_payload, headers=self.headers_, cookies=self.cookies, timeout=timeout, proxies=proxy_scheme)
+
+            except requests.exceptions.ConnectionError:
+                if attempt >= 3:
+                    return self.NO_CONNECTION
+
+                attempt += 1
+
+            else:
+                try:
+                    if 299 >= login_req.status_code >= 200:
+                        return self.SUCCESSFUL_LOGIN
+
+                    else:
+                        print(login_req.json())
+                        return self.FAILED_LOGIN
+
+                except requests.exceptions.JSONDecodeError:
+                    return self.FAILED_LOGIN
+
+                except NameError:
+                    return self.FAILED_LOGIN
+
+                except:
+                    return self.FAILED_LOGIN
---
+++ main.py
@@ -0,0 +1,6 @@
+from account import Account
+
+
+if __name__ == '__main__':
+    test_acc = Account('pixel_pug_2391__', 'eAsSx_3l$4@2ndje123@')
+    print(test_acc.login_request(5))
